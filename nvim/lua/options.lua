e305bd906c77012611d3995bc7358f21a4122e48
Sahil Katrekar <sahilkatrekar@gmail.com>
2025-12-21 14:31:39 -0800

    init

---
+++ nvim/lua/options.lua
@@ -0,0 +1,86 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Set highlight on search
+vim.opt.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.opt.termguicolors = true
+
+vim.opt.incsearch = true
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set("v", "J", ":m '>+1<Return>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<Return>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<Leader>h', ':wincmd h<Return>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>j', ':wincmd j<Return>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>k', ':wincmd k<Return>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>l', ':wincmd l<Return>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<Leader>m', "<Cmd>lua require('maximize').toggle()<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>o', "<Cmd>Oil --float<CR>", { noremap = true, silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
